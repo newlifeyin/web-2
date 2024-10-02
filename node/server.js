dc976866e9de0eac6c023d182f2a95a649b940e8
newlifeyin <3536825128@qq.com>
2024-10-02 18:26:40 +0800

    notes

--- node/server.js
+++ node/server.js
@@ -5,19 +5,19 @@ const app = express()
 app.all('*', function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");//Allow all domains to access
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");//Allow specific request headers
-	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-	res.header("X-Powered-By", ' 3.2.1')
-	res.header("Content-Type", "application/json;charset=utf-8");
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");//Permitted HTTP methods
+	res.header("X-Powered-By", ' 3.2.1')//Set X-Powered-By response header
+	res.header("Content-Type", "application/json;charset=utf-8");//Set the response content type to JSON
 	next();
 });
 
 app.use(express.json())
 
-let user = require('./api.js');
+let user = require('./api.js');//Import user related routing modules
 app.use(user);
 
 module.exports = app
-const port = 9001
+const port = 9001//Define the port on which the application runs
 app.listen(port, () => {
 	console.log(`app is running at http://localhost:${port}`)
 })
