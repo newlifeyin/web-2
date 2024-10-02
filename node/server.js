0efc5495c428354d4b521fde3d499f5840dde135
newlifeyin <3536825128@qq.com>
2024-10-02 18:22:41 +0800

    update

--- node/server.js
+++ node/server.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express')//Import Express Framework
 const app = express()
 
+//Middleware: Set response headers to support CORS and other configurations
 app.all('*', function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Origin", "*");//Allow all domains to access
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");//Allow specific request headers
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
 	res.header("X-Powered-By", ' 3.2.1')
 	res.header("Content-Type", "application/json;charset=utf-8");
